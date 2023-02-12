ec576564191f333b671740c82c74bfe3e1d52f7f
QiangQinQin <2901422401@qq.com>
2023-02-12 20:06:01 +0800

    1. 版本选择 JDK : 1.8 SpringBoot : 2.3.7.RELEASE SpringCloud : Hoxton.SR9
    2. 项目模块
    1. ldx-dependency ：
      a. pom方式;项目总依赖包;通用的依赖版本;总体父项目 （即当前模块会被其他模块依赖到）
    2. ldx-commons：
      a. jar包方式;项目公共包; 包含通用实体类,工具类,结果码等.
    3. ldx-eureka
      a. 项目注册中心;可做集群 ; 默认端口 8761;  （用不同的端口去启动，代表他是一个独立的服务 或 组件）
      b. 应用名称(spring.application.name) : ldx-eureka
      c. 端口规划为 : 7000 ; 集群规划 7000,7001,7002;
    
    4. ldx-order(理解为service层，专门处理业务的)
      a. 订单服务接口 ; 可做集群 ; 默认端口 8000
      b. 应用名称(spring.application.name) : ldx-order
      c. 端口规划 : 8000 - 8199
    5. ldx-order-api (理解为 前端发消息，就是发到api)
      a. 订单服务 ; 实际调用ldx-order ; 可做集群 ; 默认端口 8200
      b. 应用名称(spring.application.name) : ldx-order-api
      c. 端口规划 : 8200 - 8399 （为后续做集群扩展，预留的端口）
    
    6. ldx-goods
      a. 商品服务接口 ; 可做集群 ; 默认端口 8400
      b. 应用名称(spring.application.name) : ldx-goods
      c. 端口规划 : 8400 - 8599
    7. ldx-goods-api
      a. 商品服务 ; 实际调用ldx-goods; 可做集群 ; 默认端口 8600
      b. 应用名称(spring.application.name) : ldx-goods-api
      c. 端口规划 : 8600 - 8799
    
    3. 完成结果：
    先启动Eureka注册中心的服务器，再启动4个微服务，可将其注册到Eureka上
    
    4.视频：https://www.mashibing.com/study?courseNo=865&sectionNo=27828

---
+++ eureka/src/main/java/com/qq/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.qq.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  // 代表我们当前这个springboot的项目 作为一个服务注册中心 (即所有的服务都可以注册到这里面； 所有的服务都可以通过他来发现别的服务)
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
---
+++ goods/src/main/java/com/qq/GoodsApplication.java
@@ -0,0 +1,15 @@
+package com.qq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient   // 表示自己是 客户端
+public class GoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsApplication.class, args);
+    }
+
+}
